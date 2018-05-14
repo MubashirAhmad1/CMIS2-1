7bbfac1cf81a51a7b6d77b312563f01c8917e30d
MubashirAhmad1 <ahmad09tanha@gmail.com>
2018-05-14 05:13:25 +0430

    Navigation bar

---
+++ ClientApp/app/components/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'navbar',
+    templateUrl: 'navbar.component.html',
+    styleUrls:['bootstrap.min.css','navbar.component.css']
+})
+
+export class NavbarComponent{ name = 'Angular' }
